28f7ac68fe2840d820be0e5f8553905444429731
Gosia Krawczuk <gosiek.krawczuk@gmail.com>
2023-08-03 17:23:08 +0200

    crud works

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config()
 
 const app: Express = express()
+app.use(express.json())
 
 const port = process.env.PORT
 
@@ -18,14 +19,47 @@ let users = [
     },
     {
         id: 3,
-        name: "Nina",
+        name: "Sara",
     }
 ]
 // routes:
+// CREATE
+app.post("/users", (req: Request, res: Response) => {
+    const newUser = {
+        name: req.body.name,
+        id: Date.now(),
+    }
+    users.push(newUser)
+    res.json(newUser)
+})
+// READ
 app.get("/users", (req: Request, res: Response) => {
 
     res.json(users)
     
 })
 
+// UPDATE
+app.put("/users", (req: Request, res: Response) => {
+    const {name, id} = req.body;
+    users = users.map((user) => {
+        if(user.id === id){
+            user.name = name
+        } 
+        return user
+    })
+    res.json(users)
+})
+
+// DELETE
+app.delete("/users", (req: Request, res: Response) => {
+    const {id} = req.body
+    users = users.filter((user) => user.id !== id)
+    res.json(users)
+})
+
+
+
+
+// start server
 app.listen(port, () => console.log(`Listening for requests on port ${port}`));
